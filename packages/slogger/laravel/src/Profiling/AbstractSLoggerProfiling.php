5f613535f9f6e17ec3e8a5002950bd59d212d973
spp28 <sppetrovich@mail.ru>
2024-02-05 21:44:44 +0200

    removed a polling filter from AbstractSLoggerProfiling

--- packages/slogger/laravel/src/Profiling/AbstractSLoggerProfiling.php
+++ packages/slogger/laravel/src/Profiling/AbstractSLoggerProfiling.php
@@ -3,16 +3,12 @@
 namespace SLoggerLaravel\Profiling;
 
 use Illuminate\Contracts\Foundation\Application;
-use Illuminate\Support\Str;
-use SLoggerLaravel\Profiling\Dto\SLoggerProfilingObject;
 use SLoggerLaravel\Profiling\Dto\SLoggerProfilingObjects;
 
 abstract class AbstractSLoggerProfiling
 {
     private bool $profilingEnabled;
     private bool $profilingStarted = false;
-
-    private array $namespaces;
 
     abstract protected function onStart(): bool;
 
@@ -21,7 +17,6 @@ abstract class AbstractSLoggerProfiling
     public function __construct(private readonly Application $app)
     {
         $this->profilingEnabled = $this->app['config']['slogger.profiling.enabled'];
-        $this->namespaces       = $this->app['config']['slogger.profiling.namespaces'];
     }
 
     public function start(): void
@@ -44,22 +39,11 @@ abstract class AbstractSLoggerProfiling
         $filteredProfilingObjects = new SLoggerProfilingObjects();
 
         foreach ($profilingObjects->getItems() as $profilingObject) {
-            if (!$this->needProfiling($profilingObject->calling)
-                && !$this->needProfiling($profilingObject->callable)
-            ) {
-                continue;
-            }
-
             $filteredProfilingObjects->add($profilingObject);
         }
 
         $this->profilingStarted = false;
 
         return $filteredProfilingObjects;
-    }
-
-    protected function needProfiling(string $method): bool
-    {
-        return Str::is($this->namespaces, $method);
     }
 }
