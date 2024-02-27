6deb659e035bfe264f78b1f5c9873795ffcb77b3
spp28 <sppetrovich@mail.ru>
2024-02-27 15:09:58 +0200

    deleted queues.creating config

--- config/queue.php
+++ config/queue.php
@@ -109,11 +109,4 @@ return [
         'database' => env('DB_CONNECTION', 'mysql'),
         'table'    => 'failed_jobs',
     ],
-
-    'queues' => [
-        'creating' => [
-            'connection' => env('QUEUE_TRACES_CREATING_CONNECTION', 'roadrunner'),
-            'name'       => env('QUEUE_TRACES_CREATING_NAME', 'traces-creating'),
-        ],
-    ],
 ];
