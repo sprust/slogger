a8d0d81668cb0c5f7fcfc66471f75b90aa987326
spp28 <sppetrovich@mail.ru>
2024-04-02 22:12:04 +0200

    aggregator: edited text for a too long tree exception (again)

--- app/Modules/TraceAggregator/Exceptions/TreeTooLongException.php
+++ app/Modules/TraceAggregator/Exceptions/TreeTooLongException.php
@@ -8,6 +8,6 @@ class TreeTooLongException extends Exception
 {
     public function __construct(int $limit, int $current)
     {
-        parent::__construct("The tree is too long [more $current at $limit] for to view");
+        parent::__construct("The tree is too long [$current at $limit] for to view");
     }
 }
