33ecbc830536ffc1883255338aaa79d77fe95985
spp28 <sppetrovich@mail.ru>
2024-05-23 22:34:57 +0200

    aggregator/timestamps: set timestamps steps

--- app/Modules/TraceCollector/Domain/Actions/CreateTraceTimestampsAction.php
+++ app/Modules/TraceCollector/Domain/Actions/CreateTraceTimestampsAction.php
@@ -12,20 +12,49 @@ class CreateTraceTimestampsAction
         $date = $date->clone()->setMicroseconds(0);
 
         return new TraceTimestampsObject(
-            y: $date->clone()->startOfYear(),
             m: $date->clone()->startOfMonth(),
             d: $date->clone()->startOfDay(),
-            h: $date->clone()->startOfHour(),
-            min: $date->clone()->startOfMinute(),
-            s30: $date->clone()->setSeconds(
-                $date->second - ($date->second % 30)
-            ),
-            s10: $date->clone()->setSeconds(
-                $date->second - ($date->second % 10)
-            ),
-            s5: $date->clone()->setSeconds(
-                $date->second - ($date->second % 5)
-            )
+            h12: $this->sliceHours($date, 12),
+            h4: $this->sliceHours($date, 4),
+            h: $this->sliceHours($date, 1),
+            min30: $this->sliceMinutes($date, 30),
+            min10: $this->sliceMinutes($date, 10),
+            min5: $this->sliceMinutes($date, 5),
+            min: $this->sliceMinutes($date, 1),
+            s30: $this->sliceSeconds($date, 30),
+            s10: $this->sliceSeconds($date, 10),
+            s5: $this->sliceSeconds($date, 5)
         );
+    }
+
+    public function sliceHours(Carbon $date, int $slice): Carbon
+    {
+        return $date->clone()
+            ->setHours(
+                $this->sliceValue($date->hour, $slice)
+            )
+            ->startOfHour();
+    }
+
+    public function sliceMinutes(Carbon $date, int $slice): Carbon
+    {
+        return $date->clone()
+            ->setMinutes(
+                $this->sliceValue($date->minute, $slice)
+            )
+            ->startOfMinute();
+    }
+
+    public function sliceSeconds(Carbon $date, int $slice): Carbon
+    {
+        return $date->clone()
+            ->setSeconds(
+                $this->sliceValue($date->second, $slice)
+            );
+    }
+
+    private function sliceValue(int $value, int $slice): int
+    {
+        return $value - ($value % $slice);
     }
 }
--- app/Modules/TraceCollector/Domain/Entities/Objects/TraceTimestampsObject.php
+++ app/Modules/TraceCollector/Domain/Entities/Objects/TraceTimestampsObject.php
@@ -7,10 +7,14 @@ use Illuminate\Support\Carbon;
 readonly class TraceTimestampsObject
 {
     public function __construct(
-        public Carbon $y,
         public Carbon $m,
         public Carbon $d,
+        public Carbon $h12,
+        public Carbon $h4,
         public Carbon $h,
+        public Carbon $min30,
+        public Carbon $min10,
+        public Carbon $min5,
         public Carbon $min,
         public Carbon $s30,
         public Carbon $s10,
