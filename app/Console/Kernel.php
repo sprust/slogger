b56cbb429fda7408e0e45b7bb2fe57e7b1614458
spp28 <sppetrovich@mail.ru>
2024-12-09 01:48:04 +0200

    trace: added tree refreshing to cron

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -3,6 +3,7 @@
 namespace App\Console;
 
 use App\Modules\Cleaner\Infrastructure\Jobs\ClearTracesJob;
+use App\Modules\Trace\Infrastructure\Jobs\FreshTraceTreesJob;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -14,8 +15,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule): void
     {
         $schedule->job(ClearTracesJob::class)->everyFifteenMinutes();
-        // TODO: need that? No!
-        //$schedule->job(CacheServiceStatJob::class)->everyMinute();
+        $schedule->job(FreshTraceTreesJob::class)->hourly();
     }
 
     /**
