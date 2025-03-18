16c13595de07be06ae03d9dbd99ef7e20714dbaf
spp28 <sppetrovich@mail.ru>
2025-03-18 14:04:47 +0200

    refactor MigrateFreshCommand to extend Command and use ConfirmableTrait

--- app/Console/Commands/Migrate/MigrateFreshCommand.php
+++ app/Console/Commands/Migrate/MigrateFreshCommand.php
@@ -2,15 +2,19 @@
 
 namespace App\Console\Commands\Migrate;
 
+use Illuminate\Console\Command;
+use Illuminate\Console\ConfirmableTrait;
 use Illuminate\Database\Console\Migrations\FreshCommand;
 use Illuminate\Support\Facades\DB;
 use MongoDB\Laravel\Connection;
-use Symfony\Component\Console\Attribute\AsCommand;
 use Throwable;
 
-#[AsCommand(name: 'migrate:fresh')]
-class MigrateFreshCommand extends FreshCommand
+class MigrateFreshCommand extends Command
 {
+    use ConfirmableTrait;
+
+    protected $name = 'migrate:fresh';
+
     public function handle(): int
     {
         if (!$this->confirmToProceed()) {
@@ -60,6 +64,6 @@ class MigrateFreshCommand extends FreshCommand
             }
         }
 
-        return parent::handle();
+        return $this->call(FreshCommand::class);
     }
 }
